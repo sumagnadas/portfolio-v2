b1fad8fe815299a7a065714a645a45574f5c330b
sumagnadas <sumagnadas@gmail.com>
2024-12-29 00:55:12 +0530

    Add a basic app window popup display as well as drag option on status bar.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,58 @@
 let justnow = false;
+(function () {
+    const apps = document.getElementsByClassName('app');
+    for (let app of apps) {
+        // const selec = selection.apply(null, [app.id]);
+        app.addEventListener('click', (e) => selection(app.id));
+        app.addEventListener('dblclick', showModal);
+    }
+})()
+
+let offsetX, offsetY;
+function showModal() {
+    const modals = document.getElementsByClassName('modal');
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    const title_bar = document.createElement('div');
+    title_bar.classList.add('title_bar');
+    const close_button = document.createElement('div');
+    close_button.classList.add('close')
+    modal.dragged = false;
+    const drag = function (e) {
+        if (modal.classList.contains('dragged')) {
+            offsetX = offsetX ?? e.offsetX;
+            offsetY = offsetY ?? e.offsetY;
+            modal.style.left = `${e.pageX - offsetX}px`;
+            modal.style.top = `${e.pageY - offsetY}px`;
+        }
+    }
+
+    title_bar.addEventListener('mousedown', (e) => modal.classList.add('dragged'))
+    document.body.addEventListener('mousemove', drag)// added to body as it can be dragged anywhere
+    title_bar.addEventListener('mouseup', (e) => modal.classList.remove('dragged'));
+
+    title_bar.appendChild(close_button);
+    close_button.addEventListener('click', function (e) { modal.remove() })
+    modal.appendChild(title_bar);
+    modal.appendChild(document.createTextNode('Hello'));
+    document.body.insertAdjacentElement('afterbegin', modal);
+    modal.style.display = 'block';
+
+    if (modals.length > 1) {
+        let last = document.body.childNodes[1];
+        console.log(last);
+        modal.style.top = `${last.offsetTop + 10}px`;
+        modal.style.left = `${last.offsetLeft + 10}px`;
+    }
+    else {
+        modal.style.top = `${modal.offsetTop}px`;
+        modal.style.left = `${modal.offsetLeft}px`;
+    }
+}
 function selection(id) {
     if (id) {
-        var x = document.getElementById(id);
-        var y = document.getElementsByClassName('select');
+        let x = document.getElementById(id);
+        let y = document.getElementsByClassName('select');
 
         if (y.length == 1)
             y[0].classList.remove("select");
