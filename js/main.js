6f9ef66e7c9ac14c76c4215bb188014de09162b7
sumagnadas <sumagnadas@gmail.com>
2024-12-30 00:34:31 +0530

    Add necessary comments and change modal to window.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+// this function will always be ran whenever the file is loaded 
+// no extra global variables when run this way
 (function () {
     const apps = document.getElementsByClassName('app');
-    document.body.addEventListener('mousemove', dragAndResize)// added to body as it can be dragged anywhere
+    document.body.addEventListener('mousemove', dragAndResize)// added to body as it can be dragged and resized anywhere
     document.body.addEventListener('mousedown', e => {
         const window = document.getElementById('resized');
         if (window ?? false) {
@@ -12,28 +14,37 @@
         document.body.style.cursor = 'auto';
 
         let elem = document.getElementById('resized');
-        if (elem?.id ?? false)
+        if (elem ?? false)
             elem.removeAttribute('id');
     })// stop resizing if its in a border of a window
     for (let app of apps) {
         app.addEventListener('click', (e) => selection(app.id));
-        app.addEventListener('dblclick', (e) => showModal(app.id));
+        app.addEventListener('dblclick', (e) => showWindow(app.id));
         app.addEventListener('dblclick', (e) => addApp(app.id));
     }
 })()
+
+// add app to the dock
 function addApp(id) {
-    const img = document.getElementById(id).firstElementChild.firstElementChild;
-    const navbar = document.getElementById('apps');// this is the container of the navbar extra apps
+    // the icon element going to be added to the dock
     const icon = document.createElement('div');
     icon.classList.add('icon');
     icon.id = `app-${id}`;
+
+    // the img element containing the icon
+    const img = document.getElementById(id).firstElementChild.firstElementChild;
     const new_img = document.createElement('img');
     new_img.src = img.src
     new_img.alt = img.alt;
+
+    // adding the icon to the container 
+    const navbar = document.getElementById('apps');// this is the container of the navbar extra apps
     icon.appendChild(new_img);
     navbar.appendChild(icon);
     navbar.style.borderLeftWidth = '1px';
 }
+
+// remove app from the dock
 function removeApp(id) {
     const icon = document.getElementById(`app-${id}`);
     icon.remove();
@@ -41,54 +52,71 @@ function removeApp(id) {
     if (!navbar.childNodes.length)
         navbar.style.borderLeftWidth = '0';
 }
+
 let beingResized = false;
-function resize(modal, posX, posY, sizeX, sizeY, moveX, moveY) {
-
+// resize the window accordingly
+function resize(window, posX, posY, sizeX, sizeY, moveX, moveY) {
     if (sizeX && moveX) {
-        if (posX <= modal.offsetLeft + modal.offsetWidth - 10) {
-            modal.style.width = `${modal.offsetWidth - (posX - modal.offsetLeft)}px`;
-            modal.style.left = `${posX}px`
+        // cursor on the left side
+        // dont move the window by resizing or anything if its already at the min width
+        if (posX <= window.offsetLeft + window.offsetWidth - 10) {
+            window.style.width = `${window.offsetWidth - (posX - window.offsetLeft)}px`;
+            window.style.left = `${posX}px`
         }
     }
-    else if (sizeX)
-        modal.style.width = `${modal.offsetWidth + (posX - modal.offsetLeft - modal.offsetWidth)}px`;
+    else if (sizeX) // cursor on the right side
+        window.style.width = `${window.offsetWidth + (posX - window.offsetLeft - window.offsetWidth)}px`;
     if (sizeY && moveY) {
-        if (posY <= modal.offsetTop + modal.offsetHeight - 10) {
-            modal.style.height = `${modal.offsetHeight - (posY - modal.offsetTop)}px`;
-            modal.style.top = `${posY}px`
+        // cursor on the top side
+        // dont move the window by resizing or anything if its already at the min height
+        if (posY <= window.offsetTop + window.offsetHeight - 10) {
+            window.style.height = `${window.offsetHeight - (posY - window.offsetTop)}px`;
+            window.style.top = `${posY}px`
         }
     }
-    else if (sizeY)
-        modal.style.height = `${modal.offsetHeight + (posY - modal.offsetTop - modal.offsetHeight)}px`;
+    else if (sizeY) // cursor on the bottom side
+        window.style.height = `${window.offsetHeight + (posY - window.offsetTop - window.offsetHeight)}px`;
 }
-
+// states whether only the size is increasing in X,Y axes resp (sizeX, sizeY)
+// or is the top left corner (moveX, moveY) also going to be moved as if the window is being resized
 let sizeX = false, sizeY = false, moveX = false, moveY = false;
+
+// the offset of the cursor from the window position to drag it
+// this offset stays fixed making it look like its being dragged around
+let offsetX, offsetY;
+// drag the window around or set the parameters for resizing
 function dragAndResize(e) {
-    const modals = document.getElementsByClassName('modal');
+    const windows = document.getElementsByClassName('window');
+    // if its being resized, dont do anything else like dragging and checking whether its going to be resized
     if (beingResized) {
         let elem = document.getElementById('resized');
         resize(elem, e.pageX, e.pageY, sizeX, sizeY, moveX, moveY);
     }
     else {
-        for (let modal of modals) {
-            if (modal.classList.contains('dragged')) {
-                offsetX = offsetX ?? (e.pageX - modal.offsetLeft);
-                offsetY = offsetY ?? (e.pageY - modal.offsetTop);
-                modal.style.left = `${e.pageX - offsetX}px`;
-                modal.style.top = `${e.pageY - offsetY}px`;
+        for (let window of windows) {
+            // check if a window is being dragged around and set the topleft corner respectively
+            if (window.classList.contains('dragged')) {
+                offsetX = offsetX ?? (e.pageX - window.offsetLeft);
+                offsetY = offsetY ?? (e.pageY - window.offsetTop);
+                window.style.left = `${e.pageX - offsetX}px`;
+                window.style.top = `${e.pageY - offsetY}px`;
             }
 
-            const distFromRightEdge = e.pageX - modal.offsetWidth - modal.offsetLeft;
-            const distFromLeftEdge = e.pageX - modal.offsetLeft;
-            const distFromBottomEdge = e.pageY - modal.offsetHeight - modal.offsetTop;
-            const distFromTopEdge = e.pageY - modal.offsetTop;
+            const distFromRightEdge = e.pageX - window.offsetWidth - window.offsetLeft;
+            const distFromLeftEdge = e.pageX - window.offsetLeft;
+            const distFromBottomEdge = e.pageY - window.offsetHeight - window.offsetTop;
+            const distFromTopEdge = e.pageY - window.offsetTop;
             let curs_type = '';
+
+            // check if the cursor is in a 4px border rectangle of a window
             if (distFromBottomEdge <= 4 && distFromTopEdge >= -4 && distFromRightEdge <= 4 && distFromLeftEdge >= -4) {
                 if (distFromBottomEdge <= 4 && distFromBottomEdge >= -4) {
+                    // currently on the bottom side
                     curs_type += 's';
                     sizeY = true;
                 }
                 else if (distFromTopEdge <= 4 && distFromTopEdge >= -4) {
+                    // currently on the top side
                     curs_type += 'n';
                     sizeY = true;
                     moveY = true;
@@ -97,16 +125,18 @@ function dragAndResize(e) {
                     sizeY = false; moveY = false;
                 }
                 if (distFromRightEdge <= 4 && distFromRightEdge >= 0) {
+                    // currently on the right side
                     curs_type += 'e';
                     sizeX = true;
                 }
                 else if (distFromLeftEdge >= -4 && distFromLeftEdge <= 0) {
+                    // currently on the left side
                     curs_type += 'w';
                     sizeX = true;
                     moveX = true;
                 }
                 else { sizeX = false; moveX = false; }
-                modal.id = 'resized'
+                window.id = 'resized'
             }
             if (curs_type == '') {
                 let elem = document.getElementById('resized');
@@ -122,35 +152,43 @@ function dragAndResize(e) {
     }
 }
 
-let offsetX, offsetY;
-function showModal(id) { // too much extra code, will clean later
+// show the window
+function showWindow(id) { // too much extra code, will clean later
     remove();
-    const modals = document.getElementsByClassName('modal');
-    const modal = document.createElement('div');
-    modal.classList.add('modal');
+    // Create a template app window
+    const windows = document.getElementsByClassName('window');
+    const window = document.createElement('div');
+    window.classList.add('window');
+
+    // add a basic title bar
     const title_bar = document.createElement('div');
     title_bar.classList.add('title_bar');
+    title_bar.addEventListener('mousedown', (e) => window.classList.add('dragged'))
+    document.body.addEventListener('mouseup', (e) => { window.classList.remove('dragged'); offsetX = undefined; offsetY = undefined });
+
+    // add button for closing
     const close_button = document.createElement('div');
     close_button.classList.add('close')
-    modal.dragged = false;
-    title_bar.addEventListener('mousedown', (e) => modal.classList.add('dragged'))
-    document.body.addEventListener('mouseup', (e) => { modal.classList.remove('dragged'); offsetX = undefined; offsetY = undefined });
+    close_button.addEventListener('click', function (e) { window.remove(); removeApp(id) })
 
+    // add the elements created to their respective containers
     title_bar.appendChild(close_button);
-    close_button.addEventListener('click', function (e) { modal.remove(); removeApp(id) })
-    modal.appendChild(title_bar);
-    modal.appendChild(document.createTextNode('Hello'));
-    document.body.insertAdjacentElement('beforeend', modal);
-    modal.style.display = 'block';
+    window.appendChild(title_bar);
+    window.appendChild(document.createTextNode('Hello'));
 
-    if (modals.length > 1) {
+    document.body.insertAdjacentElement('beforeend', window);
+    window.style.display = 'block';
+
+    // (for now)place the new window (10,10) distance away from the last added window
+    if (windows.length > 1) {
         let last = document.body.childNodes[document.body.childNodes.length - 2];
-        modal.style.top = `${last.offsetTop + 10}px`;
-        modal.style.left = `${last.offsetLeft + 10}px`;
+        window.style.top = `${last.offsetTop + 10}px`;
+        window.style.left = `${last.offsetLeft + 10}px`;
     }
     else {
-        modal.style.top = `${modal.offsetTop}px`;
-        modal.style.left = `${modal.offsetLeft}px`;
+        // fix the window in place
+        window.style.top = `${window.offsetTop}px`;
+        window.style.left = `${window.offsetLeft}px`;
     }
 }
 
